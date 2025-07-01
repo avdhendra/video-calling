d449eadac960dece96e585f78c92dcf2e0a22a8e
avdhendrasingh <avdhendrasingh3@gmail.com>
2025-07-02 01:52:54 +0530

    chat wrapper

--- components/ChatWrapper.tsx
+++ components/ChatWrapper.tsx
@@ -1,16 +1,16 @@
-// import { PropsWithChildren } from 'react';
-// import { StreamChat } from 'stream-chat';
-// import { Chat } from 'stream-chat-react';
+import { PropsWithChildren } from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
 
-// export const ChatWrapper = ({
-//   chatClient: client,
-//   children,
-// }: PropsWithChildren<{ chatClient?: StreamChat | null }>) => {
-//   if (!client) return <div>Loading Chat...</div>;
+export const ChatWrapper = ({
+  chatClient: client,
+  children,
+}: PropsWithChildren<{ chatClient?: StreamChat | null }>) => {
+  if (!client) return <div>Loading Chat...</div>;
 
-//   return (
-//     <Chat theme="str-chat__theme-dark" client={client}>
-//       {children}
-//     </Chat>
-//   );
-// };
+  return (
+    <Chat theme="str-chat__theme-dark" client={client}>
+      {children}
+    </Chat>
+  );
+};
